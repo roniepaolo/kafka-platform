655df6c684a93b2e8c66de017fee4c28f15b9473
Ronie Arauco <ronie.paolo@krealo.pe>
2023-02-02 15:33:31 -0500

    Avoid reformat in docker entrypoint

--- kafka/img-build/docker-entrypoint.sh
+++ kafka/img-build/docker-entrypoint.sh
@@ -2,9 +2,13 @@
 
 set -eux
 
-export KAFKA_CLUSTER_ID="$(< /cluster_id.txt)"
-./bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c ./config/kraft/server.properties
-unset KAFKA_CLUSTER_ID
+kraft_logs=/kafka/data/kraft-combined-logs
+
+if [ -z "$(ls -A $kraft_logs)" ]; then
+  export KAFKA_CLUSTER_ID="$(< /cluster_id.txt)"
+  ./bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c ./config/kraft/server.properties
+  unset KAFKA_CLUSTER_ID
+fi
 
 exec ./bin/kafka-server-start.sh ./config/kraft/server.properties
 
